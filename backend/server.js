1b17ae93c24cd97396c0f19abcbca835ad51837d
mahesh kumar chhedal <mheskshedal11@gmail.com>
2025-12-12 12:56:42 +0545

    connect database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import app from "./src/app.js";
 import dotenv from 'dotenv'
+import dbConnect from "./src/db/db.js";
 dotenv.config()
 
 const PORT = process.env.PORT || 8000
+
+//connect db
+dbConnect()
 
 app.listen(PORT, () => {
     console.log(`server is running http://localhost:${PORT}`)
